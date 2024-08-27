53079e8b0c0076cc42cdd6749ff3c86b91373fd9
lakshmirajeev.p <lakshmirajeev.p@gmail.com>
2024-08-27 18:10:09 +0530

    Initial commit

---
+++ detect/Drowsiness_Detection.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, Response
+import cv2
+import imutils
+import dlib
+from scipy.spatial import distance
+from imutils import face_utils
+from pygame import mixer
+
+app = Flask(__name__)
+
+mixer.init()
+mixer.music.load("detect/music.wav")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def gen_frames():  
+    thresh = 0.25
+    frame_check = 20
+    detect = dlib.get_frontal_face_detector()
+    predict = dlib.shape_predictor("detect/models/shape_predictor_68_face_landmarks.dat")
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    cap = cv2.VideoCapture(0)
+    flag = 0
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        if len(frame.shape) > 2 and frame.shape[2] == 3:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = frame
+
+        frame = imutils.resize(frame, width=450)
+        subjects = detect(gray, 0)
+        for subject in subjects:
+            shape = predict(gray, subject)
+            shape = face_utils.shape_to_np(shape)
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            if ear < thresh:
+                flag += 1
+                if flag >= frame_check:
+                    cv2.putText(frame, "****************ALERT!****************", (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(frame, "****************ALERT!****************", (10, 325),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    mixer.music.play()
+            else:
+                flag = 0
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ drowsyDriver/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drowsyDriver project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drowsyDriver.settings')
+
+application = get_wsgi_application()
